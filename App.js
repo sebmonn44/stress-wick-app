34d73dd49944c527e498be76e9f1063058cd7bb2
sebmnoon44 <sebastienmunozfr@gmail.com>
2023-02-20 16:40:47 +0000

    add recoil

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import MyTrain from './MyTrain';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { QueryClient, QueryClientProvider } from "react-query";
+import MyTrain from "./MyTrain";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
+import sweetTheme from "./custom-theme.json";
 
 const queryClient = new QueryClient();
 
-
 export default function App() {
-
   return (
-    <QueryClientProvider client={queryClient}>
-    <View style={styles.container}>
-      <MyTrain />
-      <StatusBar style="auto" />
-    </View>
-    </QueryClientProvider>
+    <ApplicationProvider {...eva} theme={{ ...eva.light, ...sweetTheme }}>
+      <QueryClientProvider client={queryClient}>
+        <View style={styles.container}>
+          <MyTrain />
+          <StatusBar style="auto" />
+        </View>
+      </QueryClientProvider>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: `#fffafa`,
   },
 });
